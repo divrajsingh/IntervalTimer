1e220a602a5a083ec076b64081f496f427d0c938
Divraj Singh <divrajsingh@hotmail.com>
2020-05-15 21:15:19 +1000

    First

---
+++ IntervalTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IntervalTimer
+//
+//  Created by Divraj Singh on 15/5/20.
+//  Copyright © 2020 Divraj Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
